43ae8069b49ebbf0437a5db74d450ab7e1d70fc1
mie998 <nishiwaki.kyoto@gmail.com>
2023-05-30 22:28:12 +0900

    bug fix

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ INSTALL_DIR="$HOME/Work/dotfiles"
 
 if [ -d "$INSTALL_DIR" ]; then
     echo "Updating dotfiles..."
-    # idk, but on macos-latest actions file, $INSTALL_DIR always exists and is not a git repository
+    # idk the reason, but on macos-latest actions file, $INSTALL_DIR always exists and is not a git repository
     if ! git -C "$INSTALL_DIR" pull
     then
         echo "Removing unnecessary directory and renewing dotfiles directory..."
-        cd "$HOME/Work"
+        cd "$HOME/Work" || exit
         rm -rf "$INSTALL_DIR"
         git clone https://github.com/mie998/dotfiles "$INSTALL_DIR"
     fi
