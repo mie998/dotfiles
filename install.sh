9b9a39fdf8381a6a1d26de01abf7ee4da5afcdc6
mie998 <mie998@users.noreply.github.com>
2023-05-30 12:04:39 +0000

    deploy: 7264f20b7a24f44af34631791e17d3ddbf820c59

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 INSTALL_DIR="$HOME/Work/dotfiles"
 
-if [ -d "$INSTALL_DIR" ]; then
-    echo "Updating dotfiles..."
-
-    cd "$INSTALL_DIR"
-    pwd
-    ls -a
-    git status
-
-    git -C "$INSTALL_DIR" pull
-else
-    echo "Installing dotfiles..."
-    git clone https://github.com/mie998/dotfiles "$INSTALL_DIR"
-fi
+# if [ -d "$INSTALL_DIR" ]; then
+#     echo "Updating dotfiles..."
+#     git -C "$INSTALL_DIR" pull
+# else
+#     echo "Installing dotfiles..."
+#     git clone https://github.com/mie998/dotfiles "$INSTALL_DIR"
+# fi
+echo "Installing dotfiles..."
+git clone https://github.com/mie998/dotfiles "$INSTALL_DIR"
 
 /bin/bash "$INSTALL_DIR/scripts/setup.bash"
