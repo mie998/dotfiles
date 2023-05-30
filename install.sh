9d5eba08607f10f91460dc0e0a6d0fa58c74f378
mie998 <mie998@users.noreply.github.com>
2023-05-30 12:14:37 +0000

    deploy: 62974490e2d0794f9867f6a6a5d112fb5a4aa379

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 INSTALL_DIR="$HOME/Work/dotfiles"
 
-# if [ -d "$INSTALL_DIR" ]; then
-#     echo "Updating dotfiles..."
-#     git -C "$INSTALL_DIR" pull
-# else
-#     echo "Installing dotfiles..."
-#     git clone https://github.com/mie998/dotfiles "$INSTALL_DIR"
-# fi
-echo "Installing dotfiles..."
-git clone https://github.com/mie998/dotfiles "$INSTALL_DIR"
+if [ -d "$INSTALL_DIR" ]; then
+    echo "Updating dotfiles..."
+    # idk, but on macos-latest actions file, $INSTALL_DIR always exists and is not a git repository
+    if ! git -C "$INSTALL_DIR" pull
+    then
+        echo "Removing unnecessary directory and renewing dotfiles directory..."
+        rm -rf "$INSTALL_DIR"
+        git clone https://github.com/mie998/dotfiles "$INSTALL_DIR"
+    fi
+else
+    echo "Installing dotfiles..."
+    git clone https://github.com/mie998/dotfiles "$INSTALL_DIR"
+fi
 
 /bin/bash "$INSTALL_DIR/scripts/setup.bash"
