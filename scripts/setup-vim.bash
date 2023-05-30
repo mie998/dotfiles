7cd21dfd6cce1af8c0b815b8615c9c4295eb0696
mie998 <nishiwaki.kyoto@gmail.com>
2023-05-30 20:30:33 +0900

    [workflows] fix

--- scripts/setup-vim.bash
+++ scripts/setup-vim.bash
@@ -29,5 +29,6 @@ if [ -f "$XDG_DATA_HOME/nvim/site/autoload/plug.vim" ]; then
     echo "vim-plug for neovim is already installed."
 else
     echo "create a symlink of vim-plug for neovim..."
+    mkdir -p "$XDG_DATA_HOME/nvim/site/autoload"
     ln -s "$VIM_PLUG_PATH" "$XDG_DATA_HOME/nvim/site/autoload/plug.vim"
 fi
