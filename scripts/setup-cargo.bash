83b9f7ee8e42596ec5240fd3dbfaa1c19d34f9eb
mie998 <nishiwaki.kyoto@gmail.com>
2023-06-01 00:23:55 +0900

    [cargo] Lint fix

--- scripts/setup-cargo.bash
+++ scripts/setup-cargo.bash
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 set -x
 # shellcheck source=./scripts/common.bash
@@ -9,12 +8,14 @@ source "$(dirname "$0")/common.bash"
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
 
-if [ -d "$CARGO_DATA_DIR" ]; then
+if [ -d "$RUSTUP_HOME" ] && [ -d "$CARGO_HOME" ]; then
     echo "rustc and cargo is already installed."
-    # shellcheck source=/dev/null
 else
     echo "Installing rustc and cargo..."
     RUSTUP_HOME=$RUSTUP_HOME CARGO_HOME=$CARGO_HOME curl https://sh.rustup.rs -sSf | sh -s -- -y
 fi
 
+# shellcheck source=/dev/null
+source "$CARGO_HOME/env"
+sudo /bin/sh "$REPO_DIR/config/cargo/install.sh"
 
