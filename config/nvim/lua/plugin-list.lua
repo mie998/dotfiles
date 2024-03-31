6de603ed6a2d5c79a56136c8c8f3fed4f5830396
mie998 <nishiwaki.kyoto@gmail.com>
2024-03-31 19:02:57 +0900

    Changed nvim config setting method

--- config/nvim/lua/plugin-list.lua
+++ config/nvim/lua/plugin-list.lua
@@ -15,33 +15,22 @@ vim.opt.runtimepath:prepend(lazypath)
 
 --Plugin list
 require("lazy").setup({
-    {
-        "neanias/everforest-nvim",
-        version = false,
-        lazy = false,
-        priority = 1000, -- make sure to load this before all the other start plugins
-    },
+    -- color theme
+    require("plugins.everforest"),
+
     -- Common Plugin(Lua)
     'nvim-lua/plenary.nvim',
-    {'nvim-tree/nvim-web-devicons',lazy = false,priority = 1000},
+
     -- Statusline
     {'nvim-lualine/lualine.nvim'},
+
     -- Buffer Control
     {'romgrk/barbar.nvim',dependencies = { 'nvim-web-devicons' }},
-    {
-        'lambdalisue/fern.vim',
-        dependencies = {
-            { 'lambdalisue/nerdfont.vim', },
-            {
-                'lambdalisue/fern-renderer-nerdfont.vim',
-                config = function()
-                    vim.g['fern#renderer'] = "nerdfont"
-                end
-            },
-        },
-    },
     {'lambdalisue/glyph-palette.vim'},
-    {'TheLeoP/fern-renderer-web-devicons.nvim',dependencies = {'nvim-web-devicons'}}, 
+    require("plugins.fern"),
+
+    -- file explorer
+    require("plugins.neo-tree"),
 
     --Syntax Highlight
     {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
@@ -50,28 +39,20 @@ require("lazy").setup({
 
     -- Telescope
     {'nvim-telescope/telescope.nvim' },
-    {'fannheyward/telescope-coc.nvim'},
-    {
-        "nvim-telescope/telescope-frecency.nvim",
-    },
+    {"nvim-telescope/telescope-frecency.nvim"},
 
     -- Coding Support
     {'windwp/nvim-autopairs'},
-    {'windwp/nvim-ts-autotag'},
     {'haya14busa/vim-asterisk'},
     {'numToStr/Comment.nvim' },
     {'norcalli/nvim-colorizer.lua' },
     {"kylechui/nvim-surround"},
     {'simeji/winresizer' },
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
-    },
     {'prettier/vim-prettier'},
     {'tpope/vim-fugitive'},
     {'lewis6991/gitsigns.nvim'},
+    require("plugins.markdown-preview"),
+
     -- terminal Integration
     { 'akinsho/toggleterm.nvim'},
     {
--- config/nvim/lua/plugins/fern.lua
+++ config/nvim/lua/plugins/fern.lua
@@ -1,22 +1,12 @@
-vim.cmd([[
-let g:fern#renderer = "nerdfont"
-
-let g:fern#default_hidden=1
-
-nnoremap <silent><C-n> :Fern . -reveal=%<CR>
-
-augroup my-glyph-palette
-  autocmd! *
-  autocmd FileType fern call glyph_palette#apply()
-  autocmd FileType nerdtree,startify call glyph_palette#apply()
-augroup END
-
-function! s:fern_settings() abort
-  nmap <silent> <buffer> dd <Plug>(fern-action-remove)
-endfunction
-
-augroup fern-settings
-  autocmd!
-  autocmd FileType fern call s:fern_settings()
-augroup END
-]])
+return {
+  'lambdalisue/fern.vim',
+  dependencies = {
+      { 'lambdalisue/nerdfont.vim', },
+      {
+          'lambdalisue/fern-renderer-nerdfont.vim',
+          config = function()
+              vim.g['fern#renderer'] = "nerdfont"
+          end
+      },
+  },
+}
