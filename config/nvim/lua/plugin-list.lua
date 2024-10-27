b20dc0c2a64cf9bf393245bb5ff4761e195a78c5
mie998 <nishiwaki.kyoto@gmail.com>
2024-10-27 17:11:06 +0900

    setup keyMaps

--- config/nvim/lua/plugin-list.lua
+++ config/nvim/lua/plugin-list.lua
@@ -34,6 +34,26 @@ require("lazy").setup({
   -- Statusline
   { 'nvim-lualine/lualine.nvim' },
 
+  -- WhichKey
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+
   -- Buffer Control
   {
     'romgrk/barbar.nvim',
