89684598ed84a16a3f0859cbace2e501e8cf8225
mie998 <nishiwaki.kyoto@gmail.com>
2024-12-21 19:23:51 +0900

    which-key のセットアップは config-function 経由にする + sample として Telescope を整備

--- config/nvim/lua/plugins/whichkey.lua
+++ config/nvim/lua/plugins/whichkey.lua
@@ -1,44 +1,33 @@
 -- Installation with Lazy
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts_extend = { "spec", "disable.ft", "disable.bt" },
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
   opts = {
-    icons = {
-      group = vim.g.icons_enabled ~= false and "" or "+",
-      rules = false,
-      separator = "-",
-    },
-    mappings = {
-      ["<leader>f"] = {
-        name = "File",
-        f = { "<cmd>Telescope find_files<cr>", "Find File" },
-        r = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
-        n = { "<cmd>enew<cr>", "New File" },
-        s = { "<cmd>w<cr>", "Save File" },
-      },
-      ["<leader>l"] = {
-        name = "LSP",
-        f = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format" },
-        r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-        a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-      },
-    },
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = true })
-      end,
-      desc = "Buffer Local Keymaps (which-key)"
-    },
+    preset = 'modern',
   },
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
   dependencies = {
-    { 'nvim-tree/nvim-web-devicons' }
+    { 'nvim-tree/nvim-web-devicons' },
   },
+  config = function()
+    local telescope_builtin = require('telescope.builtin')
+
+    require('which-key').add({
+      -- Telescope
+      { '<leader>t',  group = 'Telescope' },
+      { '<leader>tb', telescope_builtin.buffers,                       desc = 'Open buffers' },
+      { '<leader>td', telescope_builtin.lsp_document_symbols,          desc = 'Symbols in document' },
+      { '<leader>th', telescope_builtin.help_tags,                     desc = 'Telescope help tags' },
+      { '<leader>tm', telescope_builtin.marks,                         desc = 'Telescope marks' },
+      { '<leader>to', telescope_builtin.oldfiles,                      desc = 'Recently opened files' },
+      { '<leader>ts', telescope_builtin.spell_suggest,                 desc = 'Suggest spelling' },
+      { '<leader>tt', '<cmd>TodoTelescope<CR>',                        desc = 'Todo list' },
+      { '<leader>tg', telescope_builtin.live_grep,                     desc = 'live grep with ripgrep' },
+      { '<leader>tw', telescope_builtin.lsp_dynamic_workspace_symbols, desc = 'Symbols in workspace' },
+      { '<leader>tz', '<cmd>Telescope zoxide list<CR>',                desc = 'Zoxide' }
+    })
+  end,
 }
